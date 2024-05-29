7e1b7b9c7e21e98c5ea3c18ccb42df8a9c365bd1
hour <lyvenghour@gmail.com>
2024-05-29 16:06:48 +0700

    This project build for learning with my man.

---
+++ lib/ProfileDetailPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfileDetailPage extends StatelessWidget {
+  const ProfileDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Center(
+            child: Text(
+          'Profile',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black87),
+        )),
+      ),
+    );
+  }
+}
