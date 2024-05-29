6d5e84a7ab4de70054c7877ac1bf9c0feb109519
hour <lyvenghour@gmail.com>
2024-05-29 16:21:05 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:coffeeapp/ProfileDetailPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: ProfileDetailPage(),
   ));
