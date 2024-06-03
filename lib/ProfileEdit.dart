551bff08a259926e1bdca75d930c1d326aad2ccc
phclient <sdabsong22@gmail.com>
2024-06-03 15:10:22 +0700

    add profileEdit new page

--- lib/ProfileEdit.dart
+++ lib/ProfileEdit.dart
@@ -26,31 +26,52 @@ class ProfileEdit extends StatelessWidget {
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
                   color: Colors.grey[850]),
-              height: 120,
+              height: 520,
               width: MediaQuery.of(context).size.width,
-              child: Stack(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //profile img
                   Center(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: const Image(
-                          fit: BoxFit.cover,
-                          height: 100,
-                          width: 100,
-                          image: NetworkImage(
-                              'https://scontent.fpnh10-1.fna.fbcdn.net/v/t39.30808-1/444505287_1151922775853237_877715756016378631_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGZoRoSrn0oW2zdACu0l68eO8xAMzFz5_I7zEAzMXPn8llaskMi_4Cp_wridZwWVx3bFginVhoOAVnQTEc-lG5f&_nc_ohc=nE_gILrXp20Q7kNvgHJLSvR&_nc_ht=scontent.fpnh10-1.fna&oh=00_AYCtc3W_Q1tQ64ytPRLt5sEN6JKA1iguz_luoBEj3dcIqQ&oe=6660E1B3')),
+                    child: Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(65),
+                            child: const Image(
+                              image: NetworkImage(
+                                  'https://scontent.fpnh10-1.fna.fbcdn.net/v/t39.30808-1/444505287_1151922775853237_877715756016378631_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGZoRoSrn0oW2zdACu0l68eO8xAMzFz5_I7zEAzMXPn8llaskMi_4Cp_wridZwWVx3bFginVhoOAVnQTEc-lG5f&_nc_ohc=nE_gILrXp20Q7kNvgHJLSvR&_nc_ht=scontent.fpnh10-1.fna&oh=00_AYCtc3W_Q1tQ64ytPRLt5sEN6JKA1iguz_luoBEj3dcIqQ&oe=6660E1B3'),
+                              width: 130,
+                              height: 130,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 115, left: 85),
+                          child: Container(
+                            height: 35,
+                            width: 35,
+                            decoration: const BoxDecoration(
+                              color: Colors.white70,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  //icon camera
                   const Padding(
-                    padding: EdgeInsets.only(top: 75, left: 210),
-                    child: SizedBox(
-                      child: Icon(
-                        Icons.camera_alt,
+                    padding: EdgeInsets.only(top: 10, left: 20),
+                    child: Text(
+                      'Bio',
+                      style: TextStyle(
+                        fontSize: 17,
                         color: Colors.white,
-                        size: 28,
-                        shadows: [Shadow(color: Colors.white, blurRadius: 2)],
                       ),
                     ),
                   )
