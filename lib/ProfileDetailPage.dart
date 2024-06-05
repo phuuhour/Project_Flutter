99e7dc21455109b8e76ce229191739feb9d79640
phclient <sdabsong22@gmail.com>
2024-06-05 15:44:40 +0700

    fix all have file1

--- lib/ProfileDetailPage.dart
+++ lib/ProfileDetailPage.dart
@@ -1,3 +1,4 @@
+import 'package:coffeeapp/ProfileEdit.dart';
 import 'package:flutter/material.dart';
 
 class ProfileDetailPage extends StatelessWidget {
@@ -25,58 +26,64 @@ class ProfileDetailPage extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Colors.grey[850]),
-                    height: 190,
-                    width: MediaQuery.of(context).size.width,
-                    child: Stack(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: 20, left: 20),
-                          child: Text(
-                            'Profile',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 22,
-                                color: Colors.white),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const ProfileEdit()));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.grey[850]),
+                      height: 190,
+                      width: MediaQuery.of(context).size.width,
+                      child: Stack(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(top: 20, left: 20),
+                            child: Text(
+                              'Profile',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 22,
+                                  color: Colors.white),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 70, left: 20),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(40),
-                            child: const Image(
-                                fit: BoxFit.cover,
-                                height: 80,
-                                width: 80,
-                                image: NetworkImage(
-                                    'https://scontent.fpnh10-1.fna.fbcdn.net/v/t39.30808-1/444505287_1151922775853237_877715756016378631_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGZoRoSrn0oW2zdACu0l68eO8xAMzFz5_I7zEAzMXPn8llaskMi_4Cp_wridZwWVx3bFginVhoOAVnQTEc-lG5f&_nc_ohc=nE_gILrXp20Q7kNvgHJLSvR&_nc_ht=scontent.fpnh10-1.fna&oh=00_AYCtc3W_Q1tQ64ytPRLt5sEN6JKA1iguz_luoBEj3dcIqQ&oe=6660E1B3')),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 70, left: 20),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(40),
+                              child: const Image(
+                                  fit: BoxFit.cover,
+                                  height: 80,
+                                  width: 80,
+                                  image: NetworkImage(
+                                      'https://scontent.fpnh10-1.fna.fbcdn.net/v/t39.30808-1/444505287_1151922775853237_877715756016378631_n.jpg?stp=cp6_dst-jpg_p200x200&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGZoRoSrn0oW2zdACu0l68eO8xAMzFz5_I7zEAzMXPn8llaskMi_4Cp_wridZwWVx3bFginVhoOAVnQTEc-lG5f&_nc_ohc=nE_gILrXp20Q7kNvgHJLSvR&_nc_ht=scontent.fpnh10-1.fna&oh=00_AYCtc3W_Q1tQ64ytPRLt5sEN6JKA1iguz_luoBEj3dcIqQ&oe=6660E1B3')),
+                            ),
                           ),
-                        ),
-//update
-                        const Padding(
-                          padding: EdgeInsets.only(top: 80, left: 120),
-                          child: Text(
-                            'LY VENGHOUR',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500),
+                          //update
+                          const Padding(
+                            padding: EdgeInsets.only(top: 80, left: 120),
+                            child: Text(
+                              'LY VENGHOUR',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500),
+                            ),
                           ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 115, left: 120),
-                          child: Text(
-                            'Content creator, Public person',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 115, left: 120),
+                            child: Text(
+                              'Content creator, Public person',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
