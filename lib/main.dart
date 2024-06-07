3a2257a690b86ac09837c235addfa68394129a58
Lycody2003 <lycody006@gmail.com>
2024-06-07 23:09:13 +0700

    update new

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:coffeeapp/Authentication/Login.dart';
 import 'package:coffeeapp/DashBoard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: DashBoard(),
+    home: LoginPage(),
   ));
 }
